caa6a2548b44760c83d7b26ba464e563d203b16b
aza2111 <amnahahmad@gmail.com>
2018-10-30 23:59:05 -0400

    working on a way to get events through facebook api

--- dist/event.js
+++ dist/event.js
@@ -24,3 +24,32 @@ function getTicketmasterEvents (testing) {
 }
 
 module.exports.getTicketmasterEvents = getTicketmasterEvents
+// get list of all facebook events and limit to 10
+// work in progress
+/* var FB = require('fb')
+
+FB.api(
+  '/me/events',
+  'GET',
+  {},
+  function getFacebookEvents(result) {
+    if (result.items.length > 0) {
+      return true
+    }
+    var i = 0
+    for (i = 0; i < 10 && i < result.items.length; i++) {
+      var event = result.items[i]
+      var eventdata = {
+        'id': event['id'],
+        'name': event['name'],
+        'category': event['category'],
+        'cover': event['cover'],
+        'description': event['description'],
+        'place': event['place'],
+        'ticket_uri': event['ticket_uri'],
+        'type': 'facebook'
+      }
+      database.add('events', eventdata['id'], eventdata)
+    }
+  })
+module.exports.getFacebookEvents = getFacebookEvents */
