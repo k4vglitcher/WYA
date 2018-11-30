fa083ad7c2ad6b55219b3b651eef4e73c0732b78
bontiveros <bo2253@columbia.edu>
2018-11-30 03:23:11 -0500

    Added remove group button

--- dist/groups.js
+++ dist/groups.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
 
-    $("button.plus").on("click",function(){
+  $("button.plus").on("click",function(){
+    $("#groups").append("<div class=item><p>Group</p></div>");
+  })
 
-    $("div:last").after("<div class=item><p>Group</p></div>");
-        })
-    })
+  $("button.minus").on("click",function(){
+    $("#groups").children("div:last").remove();
+  })
+
+  })
