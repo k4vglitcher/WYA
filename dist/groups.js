9369838d112859f308a3c2b7ac876f2532b6ccec
bontiveros <bo2253@columbia.edu>
2018-12-04 04:24:57 -0500

    Saves group name and description, after inputting to the textbox.

--- dist/groups.js
+++ dist/groups.js
@@ -7,18 +7,18 @@ $(document).ready(function(){
 
   $("#submit").on("click",function(){
     $("#form1").toggle();
+
+      var value = $("input:text[name=groupName]").val();
+      var value2 = $("input:text[name=Description]").val();
+      var txt = "<div class=item><p>"+value+"</p><p>"+value2+"</p></div>"
+      $("#groups").append(txt);
+
   })
 
   $("button.minus").on("click",function(){
     $("#groups").children("div:last").remove();
   })
 
-  $('#theform').submit(function(){
-    $("input[type='submit']", this)
-      .val("Please Wait...")
-      .attr('disabled', 'disabled');
-    return true;
-  });
 
 
 
